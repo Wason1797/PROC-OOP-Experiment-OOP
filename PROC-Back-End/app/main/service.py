ab0613f565b4354ec787afb012ff73b41aaa11f0
Freddy Páez Bolaños <55595421+fpaezespe37@users.noreply.github.com>
2019-10-24 12:08:36 -0500

    Final Experiment

--- PROC-Back-End/app/main/service.py
+++ PROC-Back-End/app/main/service.py
@@ -10,7 +10,7 @@ urls = Blueprint('urls', __name__)
 
 # Ingredient Routes
 
-@urls.route('/ingredient', methods=POST)
+@urls.route('/ingredient/ingredients', methods=POST)
 def create_ingredient():
     try:
         ingredient_serializer = IngredientSerializer()
@@ -37,7 +37,7 @@ def update_ingredient():
 
 @urls.route('/ingredient/id/<_id>', methods=GET)
 def get_ingredient_by_id(_id):
-    ingredient = Ingredient()
+    ingredient = Ingredient.query.get(_id)
     ingredient_serializer = IngredientSerializer()
     return ingredient_serializer.jsonify(ingredient) if ingredient._id else Response(status=404)
 
@@ -83,7 +83,10 @@ def get_size_by_id(_id):
     size_serializer = SizeSerializer()
     return size_serializer.jsonify(size) if size else Response(status=404)
 
-
+@urls.route('/size', methods=GET)
+def get_size():
+    result = get_all(Size, SizeSerializer)
+    return jsonify(result) 
 # Order Routes
 
 @urls.route('/order', methods=POST)
@@ -93,9 +96,9 @@ def create_order():
         if check_required_keys(('client_name', 'client_dni', 'client_address', 'client_phone', 'size'), request.json):
 
             client_name = request.json.get('client_name')
-            client_dni = None
-            client_address = None
-            client_phone = None
+            client_dni = request.json.get('client_dni')
+            client_address = request.json.get('client_address')
+            client_phone = request.json.get('client_phone')
             size_id = int(request.json.get('size'))
             ingredients = request.json.get('ingredients')
 
@@ -138,4 +141,4 @@ def get_orders():
 def get_order_by_id(_id):
     order = Order()
     order_serializer = OrderSerializer()
-    return order_serializer.jsonify({}) if order else Response(status=404)
+    return order_serializer.jsonify(order) if order._id else Response(status=404)
