36b4b320a447cad3b39f8c3a97c8ae8e9c3e0de3
EmilioHermosa <emiliogatop2808@gmail.com>
2019-10-24 12:07:48 -0500

    Emilio Hermosa

--- PROC-Back-End/app/main/service.py
+++ PROC-Back-End/app/main/service.py
@@ -37,9 +37,9 @@ def update_ingredient():
 
 @urls.route('/ingredient/id/<_id>', methods=GET)
 def get_ingredient_by_id(_id):
-    ingredient = Ingredient()
+    ingredient = Ingredient.query.get(_id)
     ingredient_serializer = IngredientSerializer()
-    return ingredient_serializer.jsonify(ingredient) if ingredient._id else Response(status=404)
+    return ingredient_serializer.jsonify(ingredient) if ingredient else Response(status=404)
 
 
 @urls.route('/ingredient', methods=GET)
@@ -53,7 +53,6 @@ def get_ingredients():
 @urls.route('/size', methods=POST)
 def create_size():
     try:
-
         size_serializer = SizeSerializer()
         new_size = size_serializer.load(request.json)
         db.session.add(new_size)
@@ -70,7 +69,6 @@ def update_size():
         size.name = request.json.get('name') or size.name
         size.price = request.json.get('price') or size.price
         db.session.commit()
-
         size_serializer = SizeSerializer()
         return size_serializer.jsonify(size)
     except Exception:
@@ -84,7 +82,13 @@ def get_size_by_id(_id):
     return size_serializer.jsonify(size) if size else Response(status=404)
 
 
+@urls.route('/size', methods=GET)
+def get_sizes():
+    result = get_all(Size, SizeSerializer)
+    return jsonify(result)
+
 # Order Routes
+
 
 @urls.route('/order', methods=POST)
 def create_order():
@@ -93,9 +97,9 @@ def create_order():
         if check_required_keys(('client_name', 'client_dni', 'client_address', 'client_phone', 'size'), request.json):
 
             client_name = request.json.get('client_name')
-            client_dni = None
-            client_address = None
-            client_phone = None
+            client_dni = request.json.get('client_dni')
+            client_address = request.json.get('client_address')
+            client_phone = request.json.get('client_phone')
             size_id = int(request.json.get('size'))
             ingredients = request.json.get('ingredients')
 
@@ -112,7 +116,8 @@ def create_order():
             db_ingredients = [Ingredient.query.get(int(ingredient_id))
                               for ingredient_id in ingredients] if isinstance(ingredients, list) else []
 
-            new_order.total_price = calculate_order_price(new_order, db_ingredients)
+            new_order.total_price = calculate_order_price(
+                new_order, db_ingredients)
 
             db.session.add_all([OrderDetail(order_id=new_order._id,
                                             ingredient_id=ingredient._id,
@@ -136,6 +141,6 @@ def get_orders():
 
 @urls.route('/order/id/<_id>', methods=GET)
 def get_order_by_id(_id):
-    order = Order()
+    order = Order.query.get(_id)
     order_serializer = OrderSerializer()
-    return order_serializer.jsonify({}) if order else Response(status=404)
+    return order_serializer.jsonify(order) if order else Response(status=404)
