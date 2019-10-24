e62444cc1f852c66423a78037fd33fb6e3df7afc
Axel Pullaguari <axel_2008ec@hotmail.com>
2019-10-24 11:13:35 -0500

    Final_Experiemento
    Se desarrollo todos los mantenimientos

--- PROC-Back-End/app/main/functions.py
+++ PROC-Back-End/app/main/functions.py
@@ -8,8 +8,9 @@ def get_all(Model, Serializer):
 
 def calculate_order_price(order, ingredients):
     size_price = order.size.price
-    sum(ingredient.price for ingredient in ingredients)
-    return round(size_price, 2)
+    ingriedents_prize = sum(ingredient.price for ingredient in ingredients)
+    total = size_price+ingriedents_prize
+    return round(total, 2)
 
 
 def check_required_keys(keys: tuple, _element: dict):
