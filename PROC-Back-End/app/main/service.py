77342c22580cc68936861f7e0fc2c6aedc88911a
Jerico Ruiz <jerico313@gmail.com>
2019-10-25 11:30:56 -0600

    MC_O_02

--- PROC-Back-End/app/main/service.py
+++ PROC-Back-End/app/main/service.py
@@ -37,15 +37,18 @@ def update_ingredient():
 
 @urls.route('/ingredient/id/<_id>', methods=GET)
 def get_ingredient_by_id(_id):
-    ingredient = Ingredient()
+    ingredient = Ingredient.query.get(_id)
     ingredient_serializer = IngredientSerializer()
     return ingredient_serializer.jsonify(ingredient) if ingredient._id else Response(status=404)
+
+    
 
 
 @urls.route('/ingredient', methods=GET)
 def get_ingredients():
     result = get_all(Ingredient, IngredientSerializer)
     return jsonify(result)
+
 
 
 # Pizza Size Routes
@@ -83,7 +86,10 @@ def get_size_by_id(_id):
     size_serializer = SizeSerializer()
     return size_serializer.jsonify(size) if size else Response(status=404)
 
-
+@urls.route('/size', methods=GET)
+def get_size():
+    result = get_all(Size, SizeSerializer)
+    return jsonify(result)
 # Order Routes
 
 @urls.route('/order', methods=POST)
@@ -93,9 +99,9 @@ def create_order():
         if check_required_keys(('client_name', 'client_dni', 'client_address', 'client_phone', 'size'), request.json):
 
             client_name = request.json.get('client_name')
-            client_dni = None
-            client_address = None
-            client_phone = None
+            client_dni = request.json.get('client_dni')
+            client_address = request.json.get('client_address')
+            client_phone = request.json.get('client_name')
             size_id = int(request.json.get('size'))
             ingredients = request.json.get('ingredients')
 
@@ -127,7 +133,6 @@ def create_order():
     except Exception:
         return Response(status=400)
 
-
 @urls.route('/order', methods=GET)
 def get_orders():
     result = get_all(Order, OrderSerializer)
@@ -136,6 +141,6 @@ def get_orders():
 
 @urls.route('/order/id/<_id>', methods=GET)
 def get_order_by_id(_id):
-    order = Order()
+    order = Order.query.get(_id)
     order_serializer = OrderSerializer()
-    return order_serializer.jsonify({}) if order else Response(status=404)
+    return order_serializer.jsonify (order) if order else Response(status=404)
