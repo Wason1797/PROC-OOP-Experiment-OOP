d932498ca1b89e4d53cdcf0c68392045c6d6c23c
DarianMichael <56969322+DarianMichael@users.noreply.github.com>
2019-10-25 11:24:56 -0500

    Experiment_Python

--- PROC-Back-End/app/main/service.py
+++ PROC-Back-End/app/main/service.py
@@ -37,7 +37,7 @@ def update_ingredient():
 
 @urls.route('/ingredient/id/<_id>', methods=GET)
 def get_ingredient_by_id(_id):
-    ingredient = Ingredient()
+    ingredient = Ingredient.query.get(_id)
     ingredient_serializer = IngredientSerializer()
     return ingredient_serializer.jsonify(ingredient) if ingredient._id else Response(status=404)
 
@@ -81,7 +81,12 @@ def update_size():
 def get_size_by_id(_id):
     size = Size.query.get(_id)
     size_serializer = SizeSerializer()
-    return size_serializer.jsonify(size) if size else Response(status=404)
+    return size_serializer.jsonify(size) if size._id else Response(status=404)
+
+@urls.route('/size', methods=GET)
+def get_sizes():
+    result = get_all(Size, SizeSerializer)
+    return jsonify(result)
 
 
 # Order Routes
@@ -93,9 +98,9 @@ def create_order():
         if check_required_keys(('client_name', 'client_dni', 'client_address', 'client_phone', 'size'), request.json):
 
             client_name = request.json.get('client_name')
-            client_dni = None
-            client_address = None
-            client_phone = None
+            client_dni = request.json.get('client_dni')
+            client_address = request.json.get('client_address')
+            client_phone = request.json.get('client_phone')
             size_id = int(request.json.get('size'))
             ingredients = request.json.get('ingredients')
 
@@ -136,6 +141,6 @@ def get_orders():
 
 @urls.route('/order/id/<_id>', methods=GET)
 def get_order_by_id(_id):
-    order = Order()
+    order = Order.query.get(_id)
     order_serializer = OrderSerializer()
-    return order_serializer.jsonify({}) if order else Response(status=404)
+    return order_serializer.jsonify(order) if order._id else Response(status=404)
