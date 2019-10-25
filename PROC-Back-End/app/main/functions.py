5d63013ef5d77d2df9023bba2a08b5f72ead70d9
EdisonMauri19 <edisonmauricio00@gmail.com>
2019-10-25 10:43:22 -0500

    MC_O_02 v1.0

--- PROC-Back-End/app/main/functions.py
+++ PROC-Back-End/app/main/functions.py
@@ -8,8 +8,8 @@ def get_all(Model, Serializer):
 
 def calculate_order_price(order, ingredients):
     size_price = order.size.price
-    sum(ingredient.price for ingredient in ingredients)
-    return round(size_price, 2)
+    ingredient_price = sum(ingredient.price for ingredient in ingredients)
+    return size_price+ingredient_price
 
 
 def check_required_keys(keys: tuple, _element: dict):
