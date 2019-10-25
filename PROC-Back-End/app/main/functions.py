125f48adfefc9030af80e3f04f6e5e124b2e251c
Pablo Cadena <pacadena5@espe.edu.ec>
2019-10-25 11:39:15 -0500

    Experimento python

--- PROC-Back-End/app/main/functions.py
+++ PROC-Back-End/app/main/functions.py
@@ -8,8 +8,9 @@ def get_all(Model, Serializer):
 
 def calculate_order_price(order, ingredients):
     size_price = order.size.price
-    sum(ingredient.price for ingredient in ingredients)
-    return round(size_price, 2)
+
+    a= sum(ingredient.price for ingredient in ingredients)
+    return a + size_price
 
 
 def check_required_keys(keys: tuple, _element: dict):
